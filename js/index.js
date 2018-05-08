aa42f2600d831534fb97af2dda14d2d2d73ea620
bethelight <1598204060@qq.com>
2018-05-09 00:30:43 +0800

    hhh

--- js/index.js
+++ js/index.js
@@ -15,83 +15,61 @@ const footer = `
 $(function(){
     $("header").html(header);
     $("footer").html(footer);
-
-    // $.ajax({
-    //     type: 'GET',
-    //     url: baseURL + getNews,
-    //     header: localStorage.getItem('verification'),
-    //     data: {
-    //         'type': 0,
-    //         'status': 0,
-    //         'pageNum': 1
-    //     },
-    //     success: function(res,status){
-    //         console.log(res.data);
-    //     }
-    // })
 });
 function routing() {
     window.location.href = 'update_information.html';
 }
 var type;
 var state;
+var sumPages;
 var pageNum = 1;
 var title = new Array();
 var author = new Array();
 var time = new Array();
+var id = new Array();
 let val1 = document.getElementById("dropdownMenu").innerText;
-// console.log(val1);
-// let val2 = document.getElementById("dropdownMenu1").innerText;
-// console.log(val2);
+let val2 = document.getElementById("dropdownMenu1").innerText;
+
+function viewDetail(which) {
+    let idValue = which.getAttribute('id');
+    let state = which.getAttribute('class');
+    if(state === '0')
+        window.location.href = 'published_information.html?id=' + idValue;
+    if(state === '1')
+        window.location.href = 'deleted_information.html?id=' + idValue;
+}
 function proceedData(data){
     for(let i = 0; i < data.length; i++){
         title[i] = data[i].title;
         author[i] = data[i].author;
         time[i] = data[i].createTime.substring(0,10);
+        id[i] = i;
     }
 }
 
-function menuBtn(which) {
-    type = which.getAttribute('id');
-    let typeValue = document.getElementById(type).innerText;
-    // console.log(type);
-    // console.log(a_value);
-    document.getElementById("dropdownMenu").innerText = typeValue;
-}
-function menuBtn1(which) {
-    let status = which.getAttribute('id');
-    let statusValue = document.getElementById(status).innerText;
-    if(status === 'online')
-         state = 0;
-    if(status === 'delete')
-         state = 1;
-    // console.log(type);
-    // console.log(a_value);
-    if(document.getElementById("dropdownMenu").innerText === val1){
-        alert("请您先选择资讯类型！");
-    }
-    else {
-        document.getElementById("dropdownMenu1").innerText = statusValue;
-        $.ajax({
-            // beforeSend: function (request) {
-            //     request.setRequestHeader('Authorization', localStorage.getItem('verification'));
-            // },
-            type: 'GET',
-            headers: {'Authorization':localStorage.getItem('verification')},//Authorization
-            url: baseURL + getNews,
-            data: {
-                'type': type,
-                'status': state,
-                'pageNum': pageNum
-            },
-            success: function (res,status) {
-                let data = res.data;
-                if(res.status === 0){
-                    // console.log(res.data);
-                    proceedData(data);
-
-                    for(let i = 0; i < data.length; i++){
-                    let tempInf = `
+function ajaxRequest() {
+    $.ajax({
+        // beforeSend: function (request) {
+        //     request.setRequestHeader('Authorization', localStorage.getItem('verification'));
+        // },
+        type: 'GET',
+        headers: {'Authorization':localStorage.getItem('verification')},//Authorization
+        url: baseURL + getNews,
+        data: {
+            'type': type,
+            'status': state,
+            'pageNum': pageNum
+        },
+        success: function (res,status) {
+            let data = res.data;
+            let inf = ``;
+            if(res.status === 0){
+                sumPages = res.message;
+                $('.essay-list').empty(); //清空.essay-list 内所有内容
+                proceedData(data);
+                let tempInf;
+                for(let i = 0; i < data.length; i++){
+                    tempInf = `
                         <div class="row">
                             <div class="col-sm-5 col-md-5 col-1">
                                 <span class="inf-title">${title[i]}</span>
@@ -103,12 +81,59 @@ function menuBtn1(which) {
                                 <span class="inf-title" >${time[i]}</span>
                             </div>
                             <div class="col-sm-2 col-md-2 col-4">
-                                <span class="inf-title"><a id="${i}" style="text-decoration: none;">查看</a></span>
+                                <span class="inf-title">
+                                <a href="#" class="${state}" id="${id[i]}" onclick="viewDetail(this);return false;">查看</a>
+                                </span>
                             </div>
-                        </div> `
-                    }
+                        </div> `;
+                    inf += tempInf;
                 }
+                $('.essay-list').append(inf);
             }
-        })
+        }
+    })
+}
+function menuBtn(which) {
+    type = which.getAttribute('id');
+    let typeValue = document.getElementById(type).innerText;
+    document.getElementById("dropdownMenu").innerText = typeValue;
+    if(document.getElementById("dropdownMenu1").innerText !== val2 && document.getElementById("dropdownMenu").innerText !== val1){
+        // alert("请您先选择好资讯状态！")
+        pageNum = 1;
+        ajaxRequest(); //向后台getNews
+    }
+}
+function menuBtn1(which) {
+    let status = which.getAttribute('id');
+    let statusValue = document.getElementById(status).innerText;
+    document.getElementById("dropdownMenu1").innerText = statusValue;
+    if(status === 'online')
+         state = 0;
+    if(status === 'delete')
+         state = 1;
+    if(document.getElementById("dropdownMenu1").innerText !== val2 && document.getElementById("dropdownMenu").innerText !== val1){
+        // alert("请您先选择好资讯状态！")
+        pageNum = 1;
+        ajaxRequest(); //向后台getNews
+    }
+}
+function prev() {
+    if(pageNum === 1){
+        alert("已到达首页！");
+        return false;
+    }
+    else {
+        pageNum--;
+        ajaxRequest();
+    }
+}
+function next() {
+    if(pageNum < sumPages){
+        pageNum++;
+        ajaxRequest();
+    }
+    else {
+        alert("已到达尾页");
+        return false;
     }
 }
